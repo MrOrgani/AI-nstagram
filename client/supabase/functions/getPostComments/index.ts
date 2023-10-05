a1cf68cc400e66e5a4b7ee5043394b3e4f61b58b
Maxime Organi <maxime.organi@gmail.com>
2023-10-05 15:01:54 +0200

    Adding getPostComments into edge function

---
+++ client/supabase/functions/getPostComments/index.ts
@@ -0,0 +1,44 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { corsHeaders } from "../_shared/cors.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get("SUPABASE_URL") ?? "",
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get("SUPABASE_ANON_KEY") ?? ""
+    );
+
+    // And we can run queries in the context of our authenticated user
+    const { post_id } = await req.json();
+    const { data, error } = await supabaseClient
+      .from("comments")
+      .select(
+        `*,
+          user:user_id(*)
+        `
+      )
+      .eq("post_id", post_id)
+      .order("created_at", { ascending: true });
+
+    if (error) throw error;
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
