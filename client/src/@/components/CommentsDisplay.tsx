358e47a653a25df427896717971b461c60e757b2
Maxime Organi <maxime.organi@gmail.com>
2023-10-17 11:15:32 +0200

    adding avatar from shadcn to Comments on feed posts

--- client/src/@/components/CommentsDisplay.tsx
+++ client/src/@/components/CommentsDisplay.tsx
@@ -1,10 +1,15 @@
 import { useCallback, useEffect, useState } from "react";
 
-import { getDateFromNow } from "../lib/utils";
+import {
+  getContrastingColor,
+  getDateFromNow,
+  stringToColour,
+} from "../lib/utils";
 import { usePostContext } from "../context/PostContext";
 import { getCommentsFromPostId } from "../lib/fetch/utils";
 
 import type { Comment } from "../lib/types";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const CommentsDisplay = () => {
   const [diplayComments, setDiplayComments] = useState(false);
@@ -74,42 +79,50 @@ const CommentsDisplay = () => {
           data-testid="comments-from-post"
           className={"max-h-80 overflow-auto"}
         >
-          {currentComments.map((comment) => (
-            <div
-              data-testid="comments-on-post"
-              className="flex mb-4"
-              key={`post-${currentPost.id}-comment-${comment.comment_id}`}
-            >
-              {currentPost.user?.avatar ? (
-                <div className="min-h-10 min-w-[3rem] bg-neutral-200 rounded-full">
-                  <img
-                    src={comment.user?.avatar}
-                    alt={comment.user?.name + "avatar"}
-                    className="rounded-full w-8 h-8 "
-                  />
+          {currentComments.map((comment) => {
+            const bgColour = stringToColour(comment.user.name);
+            const color = getContrastingColor(bgColour);
+
+            return (
+              <div
+                data-testid="comments-on-post"
+                className="flex mb-4"
+                key={`post-${currentPost.id}-comment-${comment.comment_id}`}
+              >
+                <div
+                  className={`min-h-10 min-w-[3rem] ${
+                    currentPost.user?.avatar ? "bg-neutral-200" : ""
+                  } rounded-full`}
+                >
+                  <Avatar className="w-8 h-8 ">
+                    <AvatarImage
+                      src={comment.user.avatar}
+                      alt={comment.user?.name + "_avatar"}
+                    />
+                    <AvatarFallback
+                      className={`text-md font-bold`}
+                      style={{ backgroundColor: bgColour, color }}
+                    >
+                      {comment.user.name[0]}
+                    </AvatarFallback>
+                  </Avatar>
                 </div>
-              ) : (
-                <div className="min-h-10 min-w-[3rem]">
-                  <div className="h-8 w-8 rounded-full bg-green-700 flex justify-center items-center text-white text-sm font-semibold">
-                    {comment.user.name[0]}
+                <div className="ml-1">
+                  <div>
+                    <span className="text-sm font-semibold text-gray-800 mr-1">
+                      {comment.user.name}
+                    </span>
+                    <span className="text-black text-sm break-words">
+                      {comment.text}
+                    </span>
                   </div>
+                  <p className="text-xs text-[#737373]">
+                    {getDateFromNow(comment.created_at)}
+                  </p>
                 </div>
-              )}
-              <div className="ml-1">
-                <div>
-                  <span className="text-sm font-semibold text-gray-800 mr-1">
-                    {comment.user.name}
-                  </span>
-                  <span className="text-black text-sm break-words">
-                    {comment.text}
-                  </span>
-                </div>
-                <p className="text-xs text-[#737373]">
-                  {getDateFromNow(comment.created_at)}
-                </p>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       )}
     </>
--- client/src/@/lib/utils.ts
+++ client/src/@/lib/utils.ts
@@ -84,6 +84,35 @@ function auto_grow(event: React.ChangeEvent<HTMLTextAreaElement>) {
   }
 }
 
+const stringToColour = (str: string) => {
+  let hash = 0;
+  str.split("").forEach((char) => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
+  });
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += value.toString(16).padStart(2, "0");
+  }
+  return colour;
+};
+
+const getContrastingColor = (color: string) => {
+  // Remove the '#' character if it exists
+  const hex = color.replace("#", "");
+
+  // Convert the hex string to RGB values
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+
+  // Calculate the brightness of the color
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  // Return black or white depending on the brightness
+  return brightness > 128 ? "black" : "white";
+};
+
 export {
   auto_grow,
   signUpUser,
@@ -94,4 +123,6 @@ export {
   getDateFromNow,
   getFormatedDate,
   getShortenedDateFromNow,
+  stringToColour,
+  getContrastingColor,
 };
