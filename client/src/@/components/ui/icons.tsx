490ec780ee8e47fecce8eddd282440606805927f
Maxime Organi <maxime.organi@gmail.com>
2023-10-17 10:01:02 +0200

    Adding grid svg icon to Icons

--- client/src/@/components/ui/icons.tsx
+++ client/src/@/components/ui/icons.tsx
@@ -139,4 +139,72 @@ export const Icons = {
       <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
   ),
+  grid: (props: IconProps) => (
+    <svg
+      aria-label="grid-icon"
+      color="rgb(0, 0, 0)"
+      fill="rgb(0, 0, 0)"
+      height="12"
+      role="img"
+      viewBox="0 0 24 24"
+      width="12"
+      {...props}
+    >
+      <rect
+        fill="none"
+        height="18"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        width="18"
+        x="3"
+        y="3"
+      ></rect>
+      <line
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        x1="9.015"
+        x2="9.015"
+        y1="3"
+        y2="21"
+      ></line>
+      <line
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        x1="14.985"
+        x2="14.985"
+        y1="3"
+        y2="21"
+      ></line>
+      <line
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        x1="21"
+        x2="3"
+        y1="9.015"
+        y2="9.015"
+      ></line>
+      <line
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        x1="21"
+        x2="3"
+        y1="14.985"
+        y2="14.985"
+      ></line>
+    </svg>
+  ),
 };
