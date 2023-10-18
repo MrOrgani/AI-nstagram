7a3c15048c22d702862f7441d317da5416c45600
Maxime Organi <maxime.organi@gmail.com>
2023-10-18 19:35:02 +0200

    Taking into account current profile user instead of connected user on Profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,36 +1,66 @@
 import React, { useEffect, useState } from "react";
 import useAuthStore from "../store/authStore";
 import { getPostsByUserId } from "../@/lib/fetch/utils";
-import { PostType } from "../@/lib/types";
+import { PostType, User } from "../@/lib/types";
 
 import ProfilePost from "../@/components/ProfilePost";
 import { ProfileHeader } from "../@/components/ProfileHeader";
 import { Icons } from "../@/components/ui/icons";
+import { useParams } from "react-router-dom";
+import supabase from "../supabase";
 
 const Profile = () => {
-  const { userProfile } = useAuthStore();
+  // const { userProfile } = useAuthStore();
+  const [currentUserProfile, setCurrentUserProfile] = useState<User | null>(
+    null
+  );
   const [currentUserPosts, setCurrentUserPosts] = useState<PostType[]>([]);
 
+  const { id: userId } = useParams();
+
   useEffect(() => {
-    if (!userProfile) {
+    if (!userId) {
       return;
     }
-    const getPost = async () => {
-      const posts = await getPostsByUserId(userProfile?.id);
-      setCurrentUserPosts(posts || []);
+    const fetchUserProfile = async () => {
+      const { data: userProfile, error: userError } = await supabase
+        .from("profiles")
+        .select("*, posts(*)")
+        .eq("user_id", userId)
+        .single();
+
+      if (userError) {
+        console.error(userError);
+        return;
+      }
+
+      setCurrentUserProfile(userProfile);
+
+      const userPosts = userProfile?.posts ?? [];
+      setCurrentUserPosts(userPosts);
     };
-    getPost();
-  }, [userProfile]);
+
+    fetchUserProfile();
+  }, [userId]);
+
+  console.log(currentUserPosts);
+
+  if (!currentUserProfile) {
+    return null;
+  }
   return (
     <main className="py-6 px-4 mx-auto min-w-[320px] max-w-[832px] ">
-      <div className="mt-20 bg-green-400 h-full">
+      <div className="mt-20 h-full">
         <ProfileHeader
           {...{
-            name: userProfile?.user_metadata?.full_name,
-            picture: userProfile?.user_metadata?.picture || userProfile?.avatar,
+            user: currentUserProfile,
             nbOfPosts: currentUserPosts.length,
           }}
-        />
+        >
+          <button className="text-sm font-semibold text-[#0095f6]">
+            Edit Profile
+          </button>
+        </ProfileHeader>
         <div className="border-t border-[#dbdbdb] flex justify-center">
           <a className="flex items-center uppercase h-[52px]">
             <Icons.grid />
@@ -41,7 +71,11 @@ const Profile = () => {
         </div>
         <div className="grid grid-cols-3 gap-2">
           {currentUserPosts.map((post) => (
-            <ProfilePost key={post.id} post={post} />
+            <ProfilePost
+              currentUserProfile={currentUserProfile}
+              key={post.id}
+              post={post}
+            />
           ))}
         </div>
       </div>
