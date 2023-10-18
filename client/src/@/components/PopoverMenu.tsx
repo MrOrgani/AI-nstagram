d5cb9856dc86e7589ca3101b7c7dde1220a89395
Maxime Organi <maxime.organi@gmail.com>
2023-10-18 16:18:35 +0200

    Moved icons from PopOverMenu into icons file

--- client/src/@/components/PopoverMenu.tsx
+++ client/src/@/components/PopoverMenu.tsx
@@ -1,5 +1,4 @@
 import { googleLogout } from "@react-oauth/google";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
   Menubar,
   MenubarContent,
@@ -10,11 +9,18 @@ import {
 } from "./ui/menubar";
 import supabase from "../../supabase";
 import useAuthStore from "../../store/authStore";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { SmallAvatar } from "./SmallAvatar";
+import { Icons } from "./ui/icons";
 
 const PopoverMenu = () => {
   const { userProfile, logout } = useAuthStore();
+
+  const navigate = useNavigate();
+
+  if (!userProfile) {
+    return null;
+  }
 
   return (
     <Menubar className="border-none">
@@ -23,21 +29,9 @@ const PopoverMenu = () => {
           <SmallAvatar user={userProfile} />
         </MenubarTrigger>
         <MenubarContent>
-          <Link to="/profile">
+          <Link to={`/${userProfile.id}`}>
             <MenubarItem>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                <circle cx="12" cy="7" r="4" />
-              </svg>
+              <Icons.profile />
               My Profile
             </MenubarItem>
           </Link>
@@ -50,27 +44,10 @@ const PopoverMenu = () => {
                 console.log(error);
               }
               logout();
+              navigate("/");
             }}
           >
-            <svg
-              fill="#000000"
-              height="24px"
-              width="24px"
-              viewBox="0 0 32 32"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <title>off</title>
-                <path d="M14.344 7.375c3.688 1.563 6.281 5.219 6.281 9.5 0 5.656-4.625 10.313-10.313 10.313-5.656 0-10.313-4.656-10.313-10.313 0-4.281 2.594-7.938 6.313-9.5v3.469c-1.938 1.313-3.25 3.5-3.25 6.031 0 4 3.25 7.25 7.25 7.25s7.25-3.25 7.25-7.25c0-2.531-1.281-4.719-3.219-6.031v-3.469zM12.031 16.813v-12.031h-3.438v12.031h3.438z"></path>{" "}
-              </g>
-            </svg>
+            <Icons.logout />
             Log out
           </MenubarItem>
         </MenubarContent>
