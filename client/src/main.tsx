8188e03859f058a0445ca1719cae7bf335e2951d
Maxime Organi <maxime.organi@gmail.com>
2023-10-26 18:23:01 +0200

    installing react-query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,17 +5,24 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./@/context/AuthContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider
       clientId={import.meta.env.VITE_PUBLIC_GOOGLE_API_TOKEN}
     >
-      <AuthContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <BrowserRouter>
+            <App />
+            <ReactQueryDevtools initialIsOpen />
+          </BrowserRouter>
+        </AuthContextProvider>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
