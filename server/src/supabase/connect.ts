0264bedb54a4e24fbb214249f5821a4812eb566a
Maxime Organi <maxime.organi@gmail.com>
2023-10-26 15:20:59 +0200

    removing server

--- server/src/supabase/connect.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const getClient = (url: string, key: string) => {
-  const supabaseClient = createClient(url, key);
-
-  return supabaseClient;
-};
-
-export default getClient;
