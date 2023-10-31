6806c1e1ad58b185cf62774e2e4966ca1339c1a7
Maxime Organi <maxime.organi@gmail.com>
2023-10-31 11:53:03 +0100

    display signIn button when no user is connect

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       </Link>
       <PostButton />
 
-      {userProfile ? <PopoverMenu /> : <LoginModal displayButton={true} />}
+      {userProfile?.id ? <PopoverMenu /> : <LoginModal displayButton={true} />}
     </header>
   );
 };
