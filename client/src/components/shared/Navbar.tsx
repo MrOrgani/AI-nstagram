a3d409f0dc5d436e6d000845ccdd6d7adc2c6070
Maxime Organi <maxime.organi@gmail.com>
2023-11-08 00:12:39 +0100

    Update Navbar component to display PostButton only on certain pages.

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -11,12 +11,14 @@ const Navbar = () => {
   const { user: userProfile } = useUserContext();
   const location = useLocation();
 
+  const displayPostButton =
+    location.pathname !== "/" && !location.pathname.includes("/edit");
   return (
     <nav className="flex-center fixed top-0 z-30 w-full shadow-navbar bg-black-pearl flex justify-between items-center sm:px-8 px-4 py-4   ">
       <Link to="/feed">
         <img src={AInstagramLogo} alt={"logo"} className="w-20 invert" />
       </Link>
-      {location.pathname !== "/" && <PostButton />}
+      {displayPostButton && <PostButton />}
 
       {userProfile?.id ? <PopoverMenu /> : <LoginModal displayButton={true} />}
     </nav>
