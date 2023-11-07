1683a51bcc21c7f49289bf33560be3b48d051786
Maxime Organi <maxime.organi@gmail.com>
2023-11-07 23:44:28 +0100

    Add framer-motion library and animation to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import robotAnimation from "@/assets/robotAnimation.json";
 import { Icons } from "@/components/ui/icons";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -10,21 +11,37 @@ const Home = () => {
         <div className="grid gap-8 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-3 text-white">
             <div className="space-y-6">
-              <h1 className="text-3xl font-bold sm:text-5xl xl:text-6xl/none ">
+              <motion.h1
+                initial={{ y: -50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ ease: "easeIn", duration: 0.5 }}
+                className="text-3xl font-bold sm:text-5xl xl:text-6xl/none ">
                 Welcome to{" "}
-                <span className="bg-gradient-to-r from-[#4ca5ff] to-[#b573f8] bg-clip-text text-transparent">
+                <motion.span
+                  initial={{ y: -50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ ease: "easeIn", duration: 0.5, delay: 0.2 }} // Add delay here
+                  className="bg-gradient-to-r from-[#4ca5ff] to-[#b573f8] bg-clip-text text-transparent">
                   AI-stagram
-                </span>
-              </h1>
-              <div className="rounded-md bg-slate-800 p-3 bg-opacity-20">
+                </motion.span>
+              </motion.h1>
+              <motion.div
+                className="rounded-md bg-slate-800 p-3 bg-opacity-20"
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ ease: "easeIn", duration: 0.5, delay: 0.5 }}>
                 <h1 className="base-regular max-w-[600px] text-zinc-500 md:text-xl dark:text-zinc-400">
                   The first social media platform where all posts are created by
                   generative AI. Explore the limitless creativity of artificial
                   intelligence.
                 </h1>
-              </div>
+              </motion.div>
             </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+            <motion.div
+              className="flex flex-col gap-2 min-[400px]:flex-row"
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ ease: "easeIn", duration: 0.5, delay: 0.7 }}>
               <Link
                 className="inline-flex items-center justify-center rounded-md bg-gradient-to-r from-[#4ca5ff] to-[#b573f8] p-1 text-sm font-medium text-black shadow transition-colors hover:bg-[#58a6ff]/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#58a6ff] disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300"
                 to={"/feed"}>
@@ -39,16 +56,20 @@ const Home = () => {
                   Learn More
                 </span>
               </Link>
-            </div>
+            </motion.div>
           </div>
-          <div className="hidden md:flex justify-center items-center overflow-hidden">
+          <motion.div
+            className="hidden md:flex justify-center items-center overflow-hidden"
+            initial={{ x: 20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ ease: "easeIn", duration: 0.5, delay: 0.7 }}>
             <Icons.spinninCircles />
             <Lottie
               animationData={robotAnimation}
               loop={true}
               className="absolute z-10 max-w-[400px] h-max-md md:w-[600px] md:h-[600px] lg:w-[800px] lg:h-[800px] "
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
