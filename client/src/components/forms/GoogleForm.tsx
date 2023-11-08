2c45c8ca38c92bc9e6938ef9eda4f0b5db2540bb
Maxime Organi <maxime.organi@gmail.com>
2023-11-08 10:43:23 +0100

    Add Google OAuth redirect URL and website URL helper function

--- client/src/components/forms/GoogleForm.tsx
+++ client/src/components/forms/GoogleForm.tsx
@@ -1,11 +1,13 @@
 import { GoogleLogin } from "@react-oauth/google";
 import supabase from "@/lib/supabase";
+import { getURL } from "@/lib/utils";
 
 export const GoogleForm = () => {
   // Handler
   const createOrGetUser = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: { redirectTo: getURL() },
     });
     if (error) {
       throw new Error("Error when connecting with Google");
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -8,6 +8,17 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import supabase from "./supabase";
 dayjs.extend(relativeTime);
 
+export const getURL = () => {
+  let url =
+    import.meta.env.VITE_WEBISTE_URL ?? // Set this to your site URL in production env.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
+
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
