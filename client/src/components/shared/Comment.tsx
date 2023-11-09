e66605f3db4018f2826787225060bba2b805d9be
Maxime Organi <maxime.organi@gmail.com>
2023-11-09 10:01:38 +0100

    Refactor Comment component imports.

--- client/src/components/shared/Comment.tsx
+++ client/src/components/shared/Comment.tsx
@@ -1,9 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  getContrastingColor,
-  getDateFromNow,
-  stringToColour,
-} from "@/lib/utils";
+import { getDateFromNow } from "@/lib/utils";
 
 import type { IComment } from "@/lib/types";
 import { usePostContext } from "@/context/PostContext";
