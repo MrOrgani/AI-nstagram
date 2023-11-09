4209b3e4efe2e8dc509d2706a6bd0ad5528e2a53
Maxime Organi <maxime.organi@gmail.com>
2023-11-09 01:52:26 +0100

    Update OpenAI import and generate function in index.ts + DALLE3

--- client/supabase/functions/openai/index.ts
+++ client/supabase/functions/openai/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import * as openai from "https://deno.land/x/openai@1.2.1/mod.ts";
+import { OpenAI } from "https://deno.land/x/openai/mod.ts";
 import { corsHeaders } from "../_shared/cors.ts";
 
 const apiKey = Deno.env.get("OPENAI_API_KEY");
@@ -10,33 +10,25 @@ serve(async (req) => {
     return new Response("ok", { headers: corsHeaders });
   }
 
-  console.info("Request received: ", req.url);
-  const ai = new openai.OpenAI(apiKey ?? "");
+  const ai = new OpenAI({ apiKey: apiKey ?? "" });
 
   const { prompt } = await req.json();
-  console.info("1️⃣ prompt", prompt);
 
-  const aiResponse = await ai.createImage({
+  const aiResponse = await ai.images.generate({
     prompt,
     n: 1,
     size: "1024x1024",
-    responseFormat: "b64_json",
+    response_format: "url",
+    model: "dall-e-3",
   });
-  console.info("2️⃣ aiResponse", aiResponse);
-
   if (aiResponse.error) {
-    console.info("3️⃣ aiResponse.error", aiResponse.error.message);
-    return new Response(
-      JSON.stringify({ message: "There was an error on OpenAI server" }),
-      {
-        headers: { ...corsHeaders, "Content-Type": "application/json" },
-        status: 400,
-      }
-    );
+    return new Response(JSON.stringify(aiResponse.error), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
   }
-
   const res = new Response(
-    JSON.stringify({ photoUrl: aiResponse.data["0"].b64_json }),
+    JSON.stringify({ generatedImages: aiResponse.data }),
     {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
@@ -44,3 +36,50 @@ serve(async (req) => {
   );
   return res;
 });
+
+// import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+// import * as openai from "https://deno.land/x/openai@1.2.1/mod.ts";
+// import { corsHeaders } from "../_shared/cors.ts";
+
+// const apiKey = Deno.env.get("OPENAI_API_KEY");
+
+// serve(async (req) => {
+//   // This is needed if you're planning to invoke your function from a browser.
+//   if (req.method === "OPTIONS") {
+//     return new Response("ok", { headers: corsHeaders });
+//   }
+
+//   console.info("Request received: ", req.url);
+//   const ai = new openai.OpenAI(apiKey ?? "");
+
+//   const { prompt } = await req.json();
+//   console.info("1️⃣ prompt", prompt);
+
+//   const aiResponse = await ai.createImage({
+//     prompt,
+//     n: 1,
+//     size: "1024x1024",
+//     responseFormat: "b64_json",
+//   });
+//   console.info("2️⃣ aiResponse", aiResponse);
+
+//   if (aiResponse.error) {
+//     console.info("3️⃣ aiResponse.error", aiResponse.error.message);
+//     return new Response(
+//       JSON.stringify({ message: "There was an error on OpenAI server" }),
+//       {
+//         headers: { ...corsHeaders, "Content-Type": "application/json" },
+//         status: 400,
+//       }
+//     );
+//   }
+
+//   const res = new Response(
+//     JSON.stringify({ photoUrl: aiResponse.data["0"].b64_json }),
+//     {
+//       headers: { ...corsHeaders, "Content-Type": "application/json" },
+//       status: 200,
+//     }
+//   );
+//   return res;
+// });
